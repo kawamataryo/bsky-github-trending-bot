bc9237ebdd668964d548991c59e43a1352ab5628
kawamataryo <ba068082@gmail.com>
2023-05-12 17:31:37 +0900

    fix: callable

--- scripts/postTrends.ts
+++ scripts/postTrends.ts
@@ -3,23 +3,19 @@ import { getFunctions, httpsCallable } from "firebase/functions";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGE_SENDER_ID,
-  appId: process.env.APP_ID,
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 (async () => {
-  const functions = getFunctions();
-  const tweetGitHubTrends = httpsCallable(functions, "tweetGitHubTrends")
+  const functions = getFunctions(app);
+  const postGithubTrends = httpsCallable(functions, "callable-postGitHubTrends")
   try {
-    const result = await tweetGitHubTrends({data: {}})
+    const result = await postGithubTrends({data: {}})
     console.log(result);
   } catch (e) {
     console.error(e)
--- scripts/updateTrends.ts
+++ scripts/updateTrends.ts
@@ -3,21 +3,17 @@ import { getFunctions, httpsCallable } from "firebase/functions";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGE_SENDER_ID,
-  appId: process.env.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 (async () => {
   const functions = getFunctions(app);
-  const scrappingGitHubTrends = httpsCallable(functions, "scrappingGitHubTrends")
+  const scrappingGitHubTrends = httpsCallable(functions, "callable-scrappingGitHubTrends")
   try {
     const result = await scrappingGitHubTrends({data: {}})
     console.log(result);
